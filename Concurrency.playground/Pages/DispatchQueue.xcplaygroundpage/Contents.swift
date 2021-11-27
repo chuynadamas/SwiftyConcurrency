9e1170349e94ed696847290b80fa28c851c8bf3b
Chuy <chuynadamas@gmail.com>
2021-11-27 13:57:15 -0600

    Initial commit

---
+++ Concurrency.playground/Pages/DispatchQueue.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+class Flight {
+    let company = "Volare"
+    var availableSeats: [String] = ["1A","1B","1C"]
+    let barrierQueue: DispatchQueue = DispatchQueue(label: "barrier", attributes: .concurrent)
+    
+    func getAvailableSeats() -> [String] {
+        barrierQueue.sync(flags: .barrier) {
+            return availableSeats
+        }
+    }
+    
+    func bookSeat() -> String {
+        barrierQueue.sync(flags: .barrier) {
+            let bookedSeat = availableSeats.first ?? ""
+            availableSeats.removeFirst()
+            return bookedSeat
+        }
+    }
+}
+
+func setUp() {
+    let flight: Flight = Flight()
+    
+    let queue1: DispatchQueue = DispatchQueue(label: "queue1")
+    let queue2: DispatchQueue = DispatchQueue(label: "queue2")
+    
+    queue1.async {
+        let bookedSeat: String = flight.bookSeat()
+        print("Booked seat is \(bookedSeat)")
+    }
+    
+    queue2.async {
+        let availableSeat: [String] = flight.availableSeats
+        print("Available seats \(availableSeat)")
+    }
+}
+
+setUp()
+
