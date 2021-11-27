3b2970a367584ef4333a63a1e11f3ed513ab6855
Chuy <chuynadamas@gmail.com>
2021-11-27 17:44:09 -0600

    DispatchQueue and Actors

--- Concurrency.playground/Pages/DispatchQueue.xcplaygroundpage/Contents.swift
+++ Concurrency.playground/Pages/DispatchQueue.xcplaygroundpage/Contents.swift
@@ -3,7 +3,9 @@ import Foundation
 class Flight {
     let company = "Volare"
     var availableSeats: [String] = ["1A","1B","1C"]
-    let barrierQueue: DispatchQueue = DispatchQueue(label: "barrier", attributes: .concurrent)
+    
+    let barrierQueue: DispatchQueue = DispatchQueue(label: "barrier",
+                                                    attributes: .concurrent)
     
     func getAvailableSeats() -> [String] {
         barrierQueue.sync(flags: .barrier) {
@@ -23,10 +25,15 @@ class Flight {
 func setUp() {
     let flight: Flight = Flight()
     
-    let queue1: DispatchQueue = DispatchQueue(label: "queue1")
+    let queue1: DispatchQueue = DispatchQueue(label: "queue1", attributes: .concurrent)
     let queue2: DispatchQueue = DispatchQueue(label: "queue2")
     
     queue1.async {
+        let bookedSeat: String = flight.bookSeat()
+        print("Booked seat is \(bookedSeat)")
+    }
+    
+    queue1.sync {
         let bookedSeat: String = flight.bookSeat()
         print("Booked seat is \(bookedSeat)")
     }
--- Concurrency.playground/Pages/UsingActors.xcplaygroundpage/Contents.swift
+++ Concurrency.playground/Pages/UsingActors.xcplaygroundpage/Contents.swift
@@ -2,6 +2,43 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+actor Flight {
+    let company = "Volare"
+    var availableSeats: [String] = ["1A","1B","1C"]
+    
+    func getAvailableSeats() -> [String] {
+        return availableSeats
+    }
+    
+    func bookSeat() -> String {
+        let bookedSeat = availableSeats.first ?? ""
+        availableSeats.removeFirst()
+        return bookedSeat
+    }
+}
+
+func setUp() {
+    let flight: Flight = Flight()
+    
+    let queue1: DispatchQueue = DispatchQueue(label: "queue1")
+    let queue2: DispatchQueue = DispatchQueue(label: "queue2")
+    
+    queue1.async {
+        Task {
+            let bookedSeat: String = await flight.bookSeat()
+            print("Booked seat is \(bookedSeat)")
+        }
+    }
+    
+    queue2.async {
+        Task {
+            let availableSeat: [String] = await flight.availableSeats
+            print("Available seats \(availableSeat)")
+        }
+    }
+}
+
+setUp()
+
 
 //: [Next](@next)
